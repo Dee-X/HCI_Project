988099e0b328d2ed392130dc5ed8905ad37ac811
Duncan <blackdj3497@gmail.com>
2022-10-18 13:45:52 -0500

    djb - Find unique data values

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import pandas as pd
 
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 # Read csv and filter out rows with '?'
 def read_csv(name):
+    unique = {}
     data = pd.read_csv(name)
     df = pd.DataFrame(data)
     df.columns = [
@@ -33,12 +29,29 @@ def read_csv(name):
             break
         else:
             df = df[df[column] != ' ?']
+            unique[column] = df[column].unique()
+
+    # unique['x1'] = df.x1.unique()
+    # unique['x2'] = df.x2.unique()
+    # unique['x3'] = df.x3.unique()
+    # unique['x4'] = df.x4.unique()
+    # unique['x5'] = df.x5.unique()
+    # unique['x6'] = df.x6.unique()
+    # unique['x7'] = df.x7.unique()
+    # unique['x8'] = df.x8.unique()
+    # unique['x9'] = df.x9.unique()
+    # unique['x10'] = df.x10.unique()
+    # unique['x11'] = df.x11.unique()
+    # unique['x12'] = df.x12.unique()
+    # unique['x13'] = df.x13.unique()
+    # unique['x14'] = df.x14.unique()
 
     print(df)
+    # print(unique)
+
+    for key, values in unique.items():
+        print(key, ': ', values)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     read_csv('Human.csv')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
