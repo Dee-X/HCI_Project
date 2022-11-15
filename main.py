5aff2bd054b980c381e9ea8cf8dbfc8460dbde39
Grajera <mdgrajera@gmail.com>
2022-11-15 13:11:55 -0600

    made arrays

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 # Read csv and filter out rows with '?'
 def read_csv(name):
     unique = {}
+    records = {}
     data = pd.read_csv(name, skipinitialspace=True)
     df = pd.DataFrame(data)
     # df.columns = ["x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "Y"]
@@ -24,7 +25,25 @@ def read_csv(name):
 
     prob_above50, prob_below50 = do_mle(train)
     test_design(test, prob_above50, prob_below50)
-
+    
+    print("BREAK POINT")
+    
+    records[0] = df.x1
+    records[1] = df.x2
+    records[2] = df.x3
+    records[3] = df.x4
+    records[4] = df.x5
+    records[5] = df.x6
+    records[6] = df.x7
+    records[7] = df.x8
+    records[8] = df.x9
+    records[9] = df.x10
+    records[10] = df.x11
+    records[11] = df.x12
+    records[12] = df.x13
+    records[13] = df.x14
+    records[14] = df.Y
+    
 
 if __name__ == '__main__':
     read_csv('Human.csv')
