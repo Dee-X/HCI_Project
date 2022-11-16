a743b3236709adb5a1ab5d90704f913b2b377ea2
Duncan <blackdj3497@gmail.com>
2022-11-16 15:39:47 -0600

    djb - Finish changing data to arbitrary values

--- main.py
+++ main.py
@@ -10,18 +10,42 @@ def read_csv(name):
     df = pd.DataFrame(data)
 
     for column in df:
-        if column == 'Y':
-            break
-        else:
-            df = df[df[column] != '?']
-            unique[column] = df[column].unique()
+        df = df[df[column] != '?']
+        unique[column] = df[column].unique()
 
+    temp_df = df.copy(deep=True)
     # Split data into two different datasets
     train, test = train_test_split(df, test_size=0.3)
 
     prob_above50, prob_below50 = do_mle(train)
 
+    for (k, v), (k2, v2) in zip(prob_above50.items(), prob_below50.items()):
+        print("{:<27} {:<22} || {:<22}".format(k, '>50k', '<=50k'))
+        print('=' * 76)
+        for (k11, v11), (k22, v22) in zip(v.items(), v2.items()):
+            # print(k11, v11, k22, v22)
+            print("{:<27} {:<22} || {:<22}".format(k11, v11, v22))
+
+        print("\n")
+
     test_design(test, prob_above50, prob_below50)
+    codes = {}
+    for k, v in unique.items():
+        count = 0
+        temp = {}
+
+        for i in (range(len(v))):
+            temp[v[i]] = count
+            count += 1
+        codes[k] = temp
+
+    for i, j in temp_df.iterrows():
+        for column in temp_df.columns:
+            val = j[column]
+            temp_df.at[i, column] = codes[column][val]
+
+    print(df)
+    print(temp_df)
 
 
 if __name__ == '__main__':
--- probability.py
+++ probability.py
@@ -8,22 +8,17 @@ def do_mle(data):
     counts_below50 = {}
     prob_above50 = {}
     prob_below50 = {}
+    data_length = len(data.index)
 
     # MLE: C(val)
     for column in data:
         counts[column] = data[column].value_counts()
 
-    # print(counts)
-
-    # full_count = len(data.index)
-
     df_above50 = data[data['Y'] == '>50K.']  # C(>50K)
     df_below50 = data[data['Y'] == '<=50K.']  # C(<=50K)
 
     above_full_count = len(df_above50.index)
-    print(above_full_count)
     below_full_count = len(df_below50.index)
-    print(below_full_count)
 
     for column in df_above50:
         counts_above50[column] = df_above50[column].value_counts()
@@ -50,23 +45,21 @@ def do_mle(data):
                 below_cnt = 0
 
             # MLE: C(val, >50k)/ C(val)
-            above_prob = above_cnt / above_full_count
+            # above_prob = above_cnt / above_full_count
+            above_prob = above_cnt / data_length
             inner_ret_above50[val] = above_prob
 
             # MLE: C(val, <=50k)/ C(val)
-            below_prob = below_cnt / below_full_count
+            # below_prob = below_cnt / below_full_count
+            below_prob = below_cnt / data_length
             inner_ret_below50[val] = below_prob
         prob_above50[column] = inner_ret_above50
         prob_below50[column] = inner_ret_below50
 
-    # print("Above: \n", prob_above50['x1'], "\n\n")
-    # print("Below: \n", prob_below50, "\n\n")
     return prob_above50, prob_below50
 
 
 def test_design(data, prob_above50, prob_below50):
-    print('In testing...')
-    # print(data)
     data_length = len(data.index)
     assigned_list = []
 
@@ -76,23 +69,17 @@ def test_design(data, prob_above50, prob_below50):
         above_entropy = 0
         for column in data.columns:
             val = j[column]
-            # print("Column: ", column, val)
             if column == 'Y':
-                # print("\nColumn: ", column, val)
                 break
             above = prob_above50[column]
             below = prob_below50[column]
             above_prob = above.get(val, 0)
             below_prob = below.get(val, 0)
 
-            # print("Above prob: ", above_prob, ", Below prob:", below_prob)
-
-            # Corpus Cross Entropy: -SUM(p(x)log_2(p(x)))
+            # Corpus Cross Entropy: -1/n*SUM(log_2(p(x)))
             if above_prob != 0:
-                # above_entropy += above_prob * math.log(val, 2)
                 above_entropy += math.log(above_prob, 2)
             if below_prob != 0:
-                # below_entropy += below_prob * math.log(val, 2)
                 below_entropy += math.log(below_prob, 2)
         # break
         above_entropy = above_entropy * (-1 / data_length)
@@ -107,16 +94,8 @@ def test_design(data, prob_above50, prob_below50):
 
     find_accuracy(data)
 
-    # print("Above: \n", prob_above50, "\n\n")
-    # print("Below: \n", prob_below50, "\n\n")
-    # print("Above: \n", prob_above50['x1'], "\n\n")
-    # for column in data:
-    # for val, cnt in data[column].items():
-    # print(data)
-
 
 def find_accuracy(data):
-    acc = 0
     data_length = len(data.index)
     sum_l = 0
 
@@ -133,4 +112,4 @@ def find_accuracy(data):
 
     acc = (1/data_length) * sum_l
 
-    print(acc)
+    print("\n\nAccuracy of Model: ", acc)
